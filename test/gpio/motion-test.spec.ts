e54cac23037ab8e507b1d6fdef6955bf796b9cea
Ismael Queiroz <ismael.queiroz@solutis.com.br>
2018-01-13 02:13:31 -0300

    Initial structure, test with coverage

--- test/gpio/motion-test.spec.ts
+++ test/gpio/motion-test.spec.ts
@@ -1,12 +1,19 @@
 import {MotionService} from '../../src/service/motion.service';
 import {expect} from 'chai';
 
-describe('MotionService accelerate', () => {
-    it('should return to full throttle', () => {
+describe('MotionService accelerate/deceleration', () => {
+    it('acceleration test at 10km', () => {
         const result = new MotionService({
             speed: 100,
         });
         expect(result.speed).to.equal(100);
         expect(result.accelerate(10)).to.equal(result.speed + 10);
+    });
+    it('10km deceleration test', () => {
+        const result = new MotionService({
+            speed: 110,
+        });
+        expect(result.speed).to.equal(110);
+        expect(result.decelerate(10)).to.equal(result.speed - 10);
     });
 });
